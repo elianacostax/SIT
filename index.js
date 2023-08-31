79c5b260660d5c88d1f2a179c432cf7111d91dab
Sergio Acosta <sergioelianacosta@gmail.com>
2023-08-30 19:44:57 -0500

    asignar rol

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ $(document).ready(function() {
 
 $(document).on("click", "#btnsoporte", function(){
 
-    if ($('#rol_id').val()== 1) {
+    if ($('#usu_rol').val()== 1) {
         $('#lbltitulo').html("Acceso Soporte");
         $('#btnsoporte').html("Acceso Usuario");
-        $('#rol_id').val(2);
+        $('#usu_rol').val(2);
        // $("#imgtipo").attr("src","public/2.jpg");
     }else{
         $('#lbltitulo').html("Acceso Usuario");
         $('#btnsoporte').html("Acceso Soporte");
-        $('#rol_id').val(1);
+        $('#usu_rol').val(1);
        // $("#imgtipo").attr("src","public/1.jpg");
     }
 })
--- index.php
+++ index.php
@@ -42,7 +42,7 @@ if (isset($_POST["enviar"]) and $_POST["enviar"] == "si") {
 
                 <form class="sign-box" action="" method="post" id="login_form">
 
-                    <input type="hidden" id="rol_id" name="rol_id" value="1">
+                    <input type="hidden" id="usu_rol" name="usu_rol" value="1">
 
                     <div class="sign-avatar">
                         <img src="public/img/avatar-sign.png" alt="">
